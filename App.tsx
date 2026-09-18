cc4092549fb1cf47a1b1f3cd2a8b5ac6b5da23bd
haris afaq <haris.afaq@inara.pk>
2026-09-18 11:53:54 +0500

    Components Custom Styling

--- App.tsx
+++ App.tsx
@@ -1,30 +1,57 @@
+
 import React from "react";
 import {
   View,
-  Text,
-  SafeAreaView,
   Image,
-  
-} from "react-native"
+  Button,
+  Alert,
+  StyleSheet,
+} from "react-native";
 
-function App(){
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+function App() {
+  const showAlert = () => {
+    Alert.alert("Hello!", "Button was pressed.");
+  };
+
   return (
-    <SafeAreaView>
-    <View>
-      <Text>
-        Text here!
-      </Text>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Image
+          source={require("./assets/logo.png")}
+          style={styles.image}
+        />
 
-      <Image
-      source={require("./assets/logo.png")}
-      style={{width: 100, height: 100,}}
-      >
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Button"
+            onPress={showAlert}
+          />
+        </View>
 
-      </Image>
-    </View>
-  </SafeAreaView>
-  )
+      </View>
+    </SafeAreaProvider>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 30,
+  },
+
+  buttonContainer: {
+    paddingHorizontal: 10,
+  },
+});
 
 export default App;
+
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './app1';
 import { name as appName } from './app.json';
+import AppPro from './app1';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppPro);
